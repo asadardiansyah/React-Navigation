6d1787a8f8376b89f453448ec7f463608368981b
asadardiansyah <asad.ardi@idesolusiasia.com>
2019-02-04 09:14:01 +0700

    run ios

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/StaticAnalyzer/React/React/normal/x86_64/RCTSourceCode.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/StaticAnalyzer/React/React/normal/x86_64/RCTSourceCode.d
@@ -1,10 +1,3 @@
 dependencies: \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Modules/RCTSourceCode.m \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Modules/RCTSourceCode.h \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Base/RCTBridgeModule.h \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Base/RCTDefines.h \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Base/RCTBridge.h \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Base/RCTBridgeDelegate.h \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Base/RCTJavaScriptLoader.h \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Base/RCTFrameUpdate.h \
-  /Users/asadardiansyah/Documents/GitHub/React-Navigation/node_modules/react-native/React/Base/RCTInvalidating.h
+  /Users/isa/Documents/GitHub/React-Navigation/node_modules/react-native/React/Modules/RCTSourceCode.m \
+  /Users/isa/Documents/GitHub/React-Navigation/node_modules/react-native/React/Modules/RCTSourceCode.h
